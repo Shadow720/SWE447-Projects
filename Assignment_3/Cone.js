d7d8085f26a5e6183d26e15c818740477c34a50b
Chin Lee <chillychin360@yahoo.com>
2017-02-07 14:21:23 -0800

    Update Cone.js

--- Assignment_3/Cone.js
+++ Assignment_3/Cone.js
@@ -17,8 +17,37 @@ function init() {
     cone.render();
 }
 
-function render() {
+function render() 
+{
     gl.clear( gl.COLOR_BUFFER_BIT );
+    
+   this.count = 4;
+    
+   this.render = function () {
+      ... // bind buffers
+      var start = 0;       var count = this.count;       gl.drawArrays(gl.TRIANGLE_STRIP,           start, count);    }; 
 }
 
+var Square = 
+    { 
+        count : 4, 
+        positions : {
+            values : new Float32Array
+            ([  
+                0.0, 0.0,  // Vertex 0  
+                1.0, 0.0,  // Vertex 1  
+                1.0, 1.0,  // Vertex 2  
+                0.0, 1.0   // Vertex 3  
+            ]), 
+                
+            numComponents : 2 
+    }, 
+        
+    colors : 
+        { 
+         values : new Float32Array([ ... ]), 
+         numComponets : 3 
+        }
+};
+                           
 window.onload = init;
