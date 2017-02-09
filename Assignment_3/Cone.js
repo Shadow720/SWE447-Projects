855b00a1b567c7f75df4fc9aaae1b892cd31771d
Chin Lee <chillychin360@yahoo.com>
2017-02-09 13:01:27 -0800

    Update Cone.js

--- Assignment_3/Cone.js
+++ Assignment_3/Cone.js
@@ -1,73 +1,24 @@
 var cone = null;
+var gl = null;
 
 function init() {
-    var canvas = document.getElementById( "webgl-canvas" );
-    cone = new cone( n );
+    var canvas = document.getElementById("webgl-canvas"); 
+    gl = WebGLUtils.setupWebGL(canvas);
     
-    gl = WebGLUtils.setupWebGL( canvas );
-
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
 
     gl.clearColor( 1.0, 0.0, 0.0, 1.0 );
-
-    render();
-};
-
-function square () {    
-   this.count = 4;
-   this.positions = 
-       {
-       values : new Float32Array([          
-           0.0, 0.0,  // Vertex 0   
-           1.0, 0.0,  // Vertex 1          
-           1.0, 1.0,  // Vertex 2          
-           0.0, 1.0   // Vertex 3      
-       ]), 
-       
-       numComponents : 2
-       };    
     
-    this.colors = { 
-         values : new Float32Array([ ... ]),       
-         numComponets : 3 
-                  }; 
-                                    
-    this.render = function () 
-    {        
-    var start = 0;     
-    var count = this.count; 
-    gl.drawArrays(gl.TRIANGLE_STRIP, start, count);    
-    };                                
-                                    
-};
+       cone = new Cone(gl, 100);
+    
+    render();
+}
 
-
-var square = { 
-        count : 4, 
-        positions : {
-            values : new Float32Array
-            ([  
-                0.0, 0.0,  // Vertex 0  
-                1.0, 0.0,  // Vertex 1  
-                0.0, 1.0,  // Vertex 3  
-                1.0, 1.0   // Vertex 2  
-            ]), 
-                
-            numComponents : 2 
-    }, 
-        
-    colors : 
-        { 
-         values : new Float32Array([ ... ]), 
-         numComponets : 3 
-        }
-};
-                                    
 function render() {
- cone.render();
-};
-                           
+    gl.clear(gl.COLOR_BUFFER_BIT);
+   cone.render();
+}
 window.onload = init;
