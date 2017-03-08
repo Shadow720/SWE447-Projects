338f4ac624a15c2a0a0d7debea6648b53c975c51
Chin Lee <chillychin720@gmail.com>
2017-03-08 13:57:31 -0800

    Update Solar.js

--- Assignment_5/Solar.js
+++ Assignment_5/Solar.js
@@ -114,7 +114,7 @@ function render()
   // Specify the viewing transformation, and use it to initialize the 
   // matrix stack
 
-  V = translate(0.0, 0.0, -0.5*(near + far));
+  V = translate(0.0, 0.0, -0.9*(near + far));
   ms.load(V);  
 
   // Create a few temporary variables to make it simpler to work with
